383f70644427fb1c46332223ac1ee753c26ae8fc
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-02-12 17:52:44 -0600

    test(database): add configuracion y setup de base de datos para pruebas
    - introduce una nueva configuracion de data source para SQLite en .idea/dataSources.xml
    - agrega test_db.py para crear y conectar una base de datos de pruebas, incluyendo creación y verificación de tablas

---
+++ test/test_db.py
@@ -0,0 +1,17 @@
+from src.infrastructure.config.database import Database
+
+db = Database()
+
+# crear db
+db.create_database('test_db.db')
+
+# conectar a db
+db.connect("test_db.db")
+
+# crear tablas
+db.execute_query("""
+    CREATE TABLE IF NOT EXISTS usuarios ( id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL UNIQUE, password TEXT NOT NULL, role TEXT DEFAULT 'cliente' )
+""")
+
+# has tables?
+print(db.has_tables())

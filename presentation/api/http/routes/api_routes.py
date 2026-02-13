9902988b3d57f8ff2580ff3479dcfa06e09f45c8
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-02-13 15:12:58 -0600

    fix(request): cambia objeto DTO de pydantic a CreateUserRequest para tipar la data que llegara desde JSON

--- presentation/api/http/routes/api_routes.py
+++ presentation/api/http/routes/api_routes.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 from presentation.api.http.controllers.create_user_controller import CreateUserController
-from src.application.dto.create_user_dto import CreateUserDTO
+from presentation.api.http.controllers.requests.create_user_request import CreateUserRequest
 
 router = APIRouter()
 controller = CreateUserController()
 
 @router.post("/registro")
-async def register_user(user_dto: CreateUserDTO):
+async def register_user(user_dto: CreateUserRequest):
     
     return await controller.handle(user_dto)

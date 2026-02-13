9047c68cb7df671b9c21e16dec6a29f7c5f0fcf1
idk6w6 <miyukioo876@gmail.com>
2026-02-13 14:47:15 -0600

    Agregar manejo de errores de validación (400) en controlador

--- presentation/api/http/controllers/create_user_controller.py
+++ presentation/api/http/controllers/create_user_controller.py
@@ -1,5 +1,7 @@
 from src.bootstrap.container import Container
 from src.application.dto.create_user_dto import CreateUserDTO
+from fastapi import status
+from fastapi.responses import JSONResponse
 
 
 class CreateUserController:
@@ -8,12 +10,19 @@ class CreateUserController:
         self.use_case = self.container.create_user_use_case()
 
     async def handle(self, request_data):
-        
+        try:
         # Mapeo de los datos de la petición al DTO interno
-        user_dto = CreateUserDTO(
-            name=request_data.name,
-            email=request_data.email,
-            password=request_data.password,
-            rol=request_data.rol
+            user_dto = CreateUserDTO(
+                name=request_data.name,
+                email=request_data.email,
+                password=request_data.password,
+                rol=request_data.rol
+            )
+            self.use_case.create_client_user(user_dto)
+
+        except ValueError:
+            # Captura de la validación de la contraseña
+            return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={"detail": "Credenciales Invalidas"}
         )
-        return self.use_case.create_client_user(user_dto)

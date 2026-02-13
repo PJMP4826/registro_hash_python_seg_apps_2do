be5775b5e87bdcf1ed9b3782923fd34248db2312
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-02-12 19:44:09 -0600

    test(create_user): integra UserRepository y conexión a base de datos en prueba de creación de usuario
    - agrega UserRepository y la conexión a Database en test_create_user.py
    - actualiza la instanciación de User para incluir el rol de usuario
    - implementa la creación de usuario en la prueba para verificar el funcionamiento

--- test/test_create_user.py
+++ test/test_create_user.py
@@ -1,8 +1,12 @@
 from src.domain.entities.user import User
 from src.domain.value_objects.email import Email
 from src.domain.value_objects.password import Password
+from src.infrastructure.config.database import Database
 
 from src.infrastructure.security.bcrypt_password_hasher import BcryptPasswordHasher
+
+from src.infrastructure.repository.user_repository import UserRepository
+from src.domain.enums.user_role import UserRole
 
 hasher = BcryptPasswordHasher()
 
@@ -12,8 +16,15 @@ user = User(
     name='Fausto',
     email=Email("fausto@gmail.com"),
     password=user_password,
-    password_hasher=hasher
+    password_hasher=hasher,
+    role=UserRole.CLIENTE
 )
+
+db = Database()
+db.connect("../test_db.db")
+
+repo = UserRepository(db=db)
+repo.create_user(user=user)
 
 print(user)
 

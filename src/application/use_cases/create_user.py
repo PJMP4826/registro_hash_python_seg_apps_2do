6d7a7fab2bca7b8e5f604124bbe6466b3e68b0a0
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-02-13 17:32:09 -0600

    fix(use_case): add try-except para manejar la exception para cuando se intente crear un rol de usuario que no esta en Enum UserRole

--- src/application/use_cases/create_user.py
+++ src/application/use_cases/create_user.py
@@ -1,7 +1,6 @@
 from src.infrastructure.repository.user_repository import UserRepository
 from src.application.dto.create_user_dto import CreateUserDTO
 from src.domain.service.password_hasher import PasswordHasher
-from src.domain.service.password_hasher import PasswordHasher
 from src.domain.entities.user import User
 from src.domain.value_objects.email import Email
 from src.domain.enums.user_role import UserRole
@@ -13,26 +12,38 @@ class CreateUser:
         self.hasher = hasher
 
     def create_admin_user(self, dto: CreateUserDTO) -> bool:
+        try:
+            user_role = UserRole(dto.rol)
+        except ValueError:
+            valid_roles = ", ".join([role.value for role in UserRole])
+            raise ValueError(f"El rol {dto.rol} no es válido. Roles permitidos: {str(valid_roles)}")
+
         email = Email(dto.email)
 
         user = User.create(
             name=dto.name,
             email=email,
             password_txt=dto.password,
-            role=UserRole.ADMIN,
+            role=user_role,
             hasher=self.hasher
         )
 
         return self.repo.create_user(user=user)
 
     def create_client_user(self, dto: CreateUserDTO) -> bool:
+        try:
+            user_role = UserRole(dto.rol)
+        except ValueError:
+            valid_roles = ", ".join([role.value for role in UserRole])
+            raise ValueError(f"El rol {dto.rol} no es válido. Roles permitidos: {str(valid_roles)}")
+
         email = Email(dto.email)
 
         user = User.create(
             name=dto.name,
             email=email,
             password_txt=dto.password,
-            role=UserRole.CLIENTE,
+            role=user_role,
             hasher=self.hasher
         )
 

f41fed93a11b2bbe1af0cc464dce38360cda58d2
idk6w6 <miyukioo876@gmail.com>
2026-02-13 20:04:12 -0600

    Importacion y conexion de rutas para el cambio de contraseña en api_routes.py

--- presentation/api/http/routes/api_routes.py
+++ presentation/api/http/routes/api_routes.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from presentation.api.http.controllers.requests.create_user_request import CreateUserRequest
 from presentation.api.http.controllers.create_user_controller import CreateUserController
+from presentation.api.http.controllers.requests.change_password_request import ChangePasswordRequest
+from presentation.api.http.controllers.change_password_controller import ChangePasswordController
 
 
 router = APIRouter()
@@ -12,3 +14,8 @@ async def register_user(request: CreateUserRequest):
     controller = CreateUserController()
     return await controller.handle(request)
 
+# Cambiar la contraseña
+@router.put("/cambiar-password")
+async def change_password(request: ChangePasswordRequest):
+    controller = ChangePasswordController()
+    return await controller.handle(request)

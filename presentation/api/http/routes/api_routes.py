41730d416ffd0219988e3ab2cf5b10985bb24928
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-02-19 01:02:22 -0600

    feat(api_routes): add endpoint a la API para cambiar el rol de un usuario

--- presentation/api/http/routes/api_routes.py
+++ presentation/api/http/routes/api_routes.py
@@ -3,6 +3,8 @@ from presentation.api.http.controllers.requests.create_user_request import Creat
 from presentation.api.http.controllers.create_user_controller import CreateUserController
 from presentation.api.http.controllers.requests.change_password_request import ChangePasswordRequest
 from presentation.api.http.controllers.change_password_controller import ChangePasswordController
+from presentation.api.http.controllers.requests.change_role_request import ChangeRoleRequest
+from presentation.api.http.controllers.change_role_controller import ChangeRoleController
 
 
 router = APIRouter()
@@ -18,4 +20,9 @@ async def register_user(request: CreateUserRequest):
 @router.put("/cambiar-password")
 async def change_password(request: ChangePasswordRequest):
     controller = ChangePasswordController()
+    return await controller.handle(request)
+
+@router.put("/cambiar-role")
+async def change_password(request: ChangeRoleRequest):
+    controller = ChangeRoleController()
     return await controller.handle(request)

c8df259d9fdd48635ca5abc7985478319948ee9c
idk6w6 <miyukioo876@gmail.com>
2026-02-24 16:30:42 -0600

    feat(use_case) add caso de uso para authenticate user

---
+++ src/application/use_cases/authenticate_user.py
@@ -0,0 +1,61 @@
+from src.domain.value_objects.email import Email
+from src.domain.value_objects.password import Password
+from src.domain.enums.user_role import UserRole
+from src.infrastructure.repository.user_repository import UserRepository
+from src.application.dto.authenticate_user_dto import AuthenticateUserDTO
+from src.domain.service.password_hasher import PasswordHasher
+from src.domain.auth.verification_user import VerificationUser
+
+
+class AuthenticateUser:
+    def __init__(self, repo: UserRepository, hasher: PasswordHasher):
+        self.repo = repo
+        self.hasher = hasher
+
+    def execute(self, dto: AuthenticateUserDTO) -> bool:
+        try:
+            # Validar el formato del email desde el dominio
+            email = Email(dto.email)
+
+            # Obtener el hash guardado en la BD
+            existing_password_hash = self._get_password_hash_from_db(email=email.value)
+
+            # Reconstruccion del Objeto de Valor de la contraseña
+            existing_password = Password.create_from_hash(existing_password_hash)
+
+            self._verify_password(
+                existing_password=existing_password,
+                password_txt=dto.password_txt
+            )
+
+            return True
+
+        except ValueError as ve:
+            raise ve
+        except Exception as e:
+            raise Exception(f"Error al autenticar usuario con email {dto.email}: {str(e)}")
+        
+    
+    def _get_password_hash_from_db(self, email: str) -> str:
+        try:
+            existing_password_hash = self.repo.get_password_hash_by_email(email)
+            if not existing_password_hash:
+                raise ValueError("El usuario no existe")
+            
+            return existing_password_hash
+        except ValueError as ve:
+            raise ve
+        
+
+    def _verify_password(self, existing_password: Password, password_txt: str):
+        try:
+            verification_user = VerificationUser(
+                password=existing_password,
+                password_hasher=self.hasher
+            )   
+            if not verification_user.verify_password(password_txt=password_txt):
+                raise ValueError("Contraseña actual inválida")
+            return True
+
+        except ValueError as ve:
+            raise ve

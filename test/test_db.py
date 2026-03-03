9099833439ce3fa5b2001b2a9d609c4363afe3bc
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-03-03 10:36:29 -0600

    fix(tests): actualiza ruta de base de datos y esquema en test_db.py
    - cambia la ruta de la base de datos a './test_db_2.db' para garantizar consistencia en las pruebas
    - agrega el campo uuid al esquema de la tabla usuarios para alinearlo con la actualización de la entidad User

--- test/test_db.py
+++ test/test_db.py
@@ -3,14 +3,14 @@ from src.infrastructure.config.database import Database
 db = Database()
 
 # crear db
-db.create_database('../test_db.db')
+db.create_database('./test_db_2.db')
 
 # conectar a db
-db.connect("../test_db.db")
+db.connect("./test_db_2.db")
 
 # crear tablas
 db.execute("""
-    CREATE TABLE IF NOT EXISTS usuarios ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT NOT NULL UNIQUE, password TEXT NOT NULL, role TEXT DEFAULT 'cliente' )
+    CREATE TABLE IF NOT EXISTS usuarios ( id INTEGER PRIMARY KEY AUTOINCREMENT, uuid TEXT NOT NULL UNIQUE, name TEXT NOT NULL, email TEXT NOT NULL UNIQUE, password TEXT NOT NULL, role TEXT DEFAULT 'cliente' )
 """)
 
 # has tables?

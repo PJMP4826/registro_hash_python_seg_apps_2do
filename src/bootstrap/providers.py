2d0985d690245dfacce5757a0e5b80a1c4c5af43
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-03-03 10:37:06 -0600

    fix(providers): actualiza la ruta de la base de datos sqlite

--- src/bootstrap/providers.py
+++ src/bootstrap/providers.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 
 BASE_DIR = Path(__file__).resolve().parents[2]
-DB_PATH = BASE_DIR / "test_db.db"
+DB_PATH = BASE_DIR / "test_db_2.db"
 
 def get_db_session() -> Database:
     db = Database()

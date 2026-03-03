2f6be60cbcf3afad76375cb2c6576f867d6e4085
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-03-03 11:27:29 -0600

    feat(authenticate_user): implementa generacion de JWT en el proceso de autenticacion
    - agrega el metodo _generate_jwt_token para crear un token JWT al autenticar un usuario
    - modifica el metodo execute para retornar un AuthToken en lugar de un booleano
    - incluye la validacion y recuperacion de datos del usuario a partir del email

--- src/application/use_cases/authenticate_user.py
+++ src/application/use_cases/authenticate_user.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from src.domain.value_objects.email import Email
 from src.domain.value_objects.password import Password
 from src.domain.enums.user_role import UserRole
@@ -5,14 +6,18 @@ from src.infrastructure.repository.user_repository import UserRepository
 from src.application.dto.authenticate_user_dto import AuthenticateUserDTO
 from src.domain.service.password_hasher import PasswordHasher
 from src.domain.auth.verification_user import VerificationUser
+from src.domain.entities.token import TokenPayload
+from src.domain.value_objects.auth_token import AuthToken
+from src.domain.ports.jwt_service_port import TokenServicePort
 
 
 class AuthenticateUser:
-    def __init__(self, repo: UserRepository, hasher: PasswordHasher):
+    def __init__(self, repo: UserRepository, hasher: PasswordHasher, token_service: TokenServicePort):
         self.repo = repo
         self.hasher = hasher
+        self._token_service = token_service
 
-    def execute(self, dto: AuthenticateUserDTO) -> bool:
+    def execute(self, dto: AuthenticateUserDTO) -> AuthToken:
         try:
             # Validar el formato del email desde el dominio
             email = Email(dto.email)
@@ -23,12 +28,14 @@ class AuthenticateUser:
             # Reconstruccion del Objeto de Valor de la contraseña
             existing_password = Password.create_from_hash(existing_password_hash)
 
+            user = self.repo.get_user_by_email(email=email.value)
+
             self._verify_password(
                 existing_password=existing_password,
                 password_txt=dto.password_txt
             )
 
-            return True
+            return self._generate_jwt_token(user_data=user)
 
         except ValueError as ve:
             raise ve
@@ -59,3 +66,22 @@ class AuthenticateUser:
 
         except ValueError as ve:
             raise ve
+        
+    def _generate_jwt_token(self, user_data: dict[str, str]) -> AuthToken:
+            expiration_time = datetime.now() + timedelta(minutes=15)
+
+
+            payload = TokenPayload(
+                subject=user_data.get("uuid"),
+                claim={
+                    "email": user_data.get("email"),
+                    "role": user_data.get("role")
+                },
+                expires_at=expiration_time,
+                iat=datetime.now()
+            )
+
+            token = self._token_service.generate(payload=payload)
+
+            return token
+

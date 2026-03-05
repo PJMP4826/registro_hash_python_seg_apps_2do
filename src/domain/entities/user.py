cc3690c278e950f76e69e76c97f8c99171b357ed
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-03-05 13:37:56 -0600

    refactor(user): transforma la clase User para usar un constructor y propiedades privadas
    - mejora la encapsulación de los atributos de la clase y simplifica el acceso a las propiedades
    - actualiza la lógica de verificación y cambio de contraseña para utilizar los nuevos atributos privados

--- src/domain/entities/user.py
+++ src/domain/entities/user.py
@@ -3,31 +3,55 @@ from src.domain.service.password_hasher import PasswordHasher
 from src.domain.value_objects.email import Email
 from src.domain.value_objects.password import Password
 from src.domain.enums.user_role import UserRole
-from dataclasses import dataclass
 
 
-@dataclass()
 class User:
-    uuid: str
-    name: str
-    email: Email
-    password: Password
-    role: UserRole
+    def __init__(
+        self,
+        uuid: str,
+        name: str,
+        email: Email,
+        password: Password,
+        role: UserRole,
+    ):
+        self._uuid = uuid
+        self._name = name
+        self._email = email
+        self._password = password
+        self._role = role
 
-    # en este puento User ya tiene carga el hash de la password desde DB en password.hashed_value
-    def verify_password(self, password_txt: str, password_hasher: PasswordHasher):
-        return self.password.verify(password_txt, password_hasher)
+    # exponer propiedades read-only en python
+    @property
+    def uuid(self) -> str:
+        return self._uuid
+
+    @property
+    def email(self) -> Email:
+        return self._email
+
+    @property
+    def role(self) -> UserRole:
+        return self._role
+
+    @property
+    def password(self) -> Password:
+        return self._password
+
+    def verify_password(self, password_txt: str, password_hasher: PasswordHasher) -> bool:
+        return self._password.verify(password_txt, password_hasher)
 
     def change_password(
-        self, old_password_txt: str, new_password: str, password_hasher: PasswordHasher
-    ) -> Password:
-        if not self.password.verify(
-            password_txt=old_password_txt, hasher=password_hasher
-        ):
-            raise ValueError("Contraseña actual inválida")
+        self,
+        old_password_txt: str,
+        new_password_txt: str,
+        password_hasher: PasswordHasher,
+    ) -> None:
 
-        self.password = Password.create_from_text(
-            password_txt=new_password, hasher=password_hasher
+        if not self._password.verify(old_password_txt, password_hasher):
+            raise ValueError("Invalid current password")
+
+        self._password = Password.create_from_text(
+            password_txt=new_password_txt, hasher=password_hasher
         )
 
     @classmethod
@@ -49,17 +73,18 @@ class User:
         )
 
     def to_dict(self) -> dict:
-        """convierte a diccionario para persistencia"""
+
         return {
-            "uuid": self.uuid,
-            "name": str(self.name),
-            "email": str(self.email),
-            "password": self.password.hashed_value,
-            "role": self.role.value,
+            "uuid": self._uuid,
+            "name": self._name,
+            "email": str(self._email),
+            "password": self._password.hashed_value,
+            "role": self._role.value,
         }
 
     @classmethod
-    def from_dict(cls, data: dict) -> "User":
+    def from_dict(cls, data: dict):
+
         return cls(
             uuid=data["uuid"],
             name=data["name"],

a7093981aea0b604a585d5d37ab421fce6ba6003
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-03-05 11:18:45 -0600

    feat(database): agrega el schema de tablas necesarias para la app en el script de init_database

--- init_database.py
+++ init_database.py
@@ -28,18 +28,70 @@ def create_tables_schema(db_name: str):
     try:
         db.connect(db_name=db_name)
 
-        query = """
-            CREATE TABLE IF NOT EXISTS usuarios (
+        queries = [
+            "PRAGMA foreign_keys = ON;",
+
+            """ 
+            CREATE TABLE departamentos (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                piso TEXT NOT NULL,
+                num_departamento INTEGER NOT NULL,
+                monto_renta NUMERIC NOT NULL,
+                status TEXT NOT NULL CHECK (status IN ('ocupado','disponible'))
+            );
+            """,
+
+            """
+            CREATE TABLE inquilinos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre_completo TEXT NOT NULL,
+                telefono TEXT NOT NULL,
+                estatus INTEGER NOT NULL CHECK (estatus IN (0,1)),
+                departamento_id INTEGER NOT NULL UNIQUE,
+                FOREIGN KEY (departamento_id) REFERENCES departamentos(id)
+            );
+            """,
+
+            """
+            CREATE TABLE cuotas (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                inquilino_id INTEGER NOT NULL,
+                monto NUMERIC NOT NULL,
+                fecha TEXT NOT NULL,
+                fecha_vencimiento TEXT NOT NULL,
+                estado TEXT NOT NULL CHECK (
+                    estado IN ('pendiente','vencida','pagada','parcialmente_pagada')
+                ),
+                FOREIGN KEY (inquilino_id) REFERENCES inquilinos(id)
+            );
+            """,
+
+            """
+            CREATE TABLE pagos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                monto NUMERIC NOT NULL,
+                fecha TEXT NOT NULL,
+                cuota_id INTEGER NOT NULL,
+                FOREIGN KEY (cuota_id) REFERENCES cuotas(id)
+            );
+            """,
+
+            """
+            CREATE TABLE usuarios (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                inquilino_id INTEGER,
                 uuid TEXT NOT NULL UNIQUE,
                 name TEXT NOT NULL,
                 email TEXT NOT NULL UNIQUE,
                 password TEXT NOT NULL,
-                role TEXT DEFAULT 'cliente'
+                role TEXT NOT NULL CHECK (role IN ('admin','cliente')),
+                FOREIGN KEY (inquilino_id) REFERENCES inquilinos(id)
             );
-        """
+            """
+        ]
 
-        db.execute(query=query)
+        for query in queries:
+            db.execute(query=query)
 
         print("Schema de tablas creado")
     except Exception as e:

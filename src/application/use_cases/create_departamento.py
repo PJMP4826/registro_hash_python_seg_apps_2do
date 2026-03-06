80e5ee8115747b008182f01701a4271b8f6100a7
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-03-05 20:02:29 -0600

    feat(use_cases): implementa caso de uso para crear departamento
    - agrega la clase CreateDepartamento que gestiona la creación de instancias de Departamento
    - incluye validación del estado del departamento y manejo de excepciones
    - mejora la interacción con el repositorio de departamentos para persistir nuevos objetos

---
+++ src/application/use_cases/create_departamento.py
@@ -0,0 +1,40 @@
+from decimal import Decimal
+from src.domain.entities.departamento import Departamento
+from src.domain.enums.departamento_status import DepartamentoStatus
+from src.application.commands.create_departamento_command import (
+    CreateDepartamentoCommand,
+)
+from src.infrastructure.repository.departamento_repository import DepartamentoRepository
+
+
+class CreateDepartamento:
+    def __init__(self, departamento_repository: DepartamentoRepository) -> None:
+        self._repo = departamento_repository
+
+    def execute(self, command: CreateDepartamentoCommand) -> bool:
+        try:
+            departamento_status = DepartamentoStatus(command.status)
+        except ValueError as e:
+            valid_status = ", ".join([estatus.value for estatus in DepartamentoStatus])
+            raise ValueError(
+                f"El estatus {command.status} no es válido. estatus permitidos: {str(valid_status)}"
+            )
+
+        try:
+            departamento = Departamento.create(
+                monto_renta=Decimal(command.monto_renta),
+                num_departamento=command.num_departamento,
+                piso=command.piso,
+                status=departamento_status,
+            )
+
+            created = self._repo.create(departamento=departamento)
+
+            if created and created > 0:
+                return True
+
+            return False
+        except ValueError as ve:
+            raise ve
+        except Exception as e:
+            raise Exception(f"Error creando el departamento: {str(e)}")

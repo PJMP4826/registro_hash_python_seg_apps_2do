4a8dd8ca1c3aecd81478fdc779a6ed309933dd2d
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-03-12 13:46:54 -0600

    feat(logging): integrate logger for inquilino and departamento management
    - added logging functionality to the CreateInquilinoAndAssignDepartamento use case
    - replaced print statements with logger.info and logger.warning for better logging practices
    - improved error handling by logging warnings when a department is not available or already occupied

--- src/application/use_cases/crear_inquilino_and_assign_departamento.py
+++ src/application/use_cases/crear_inquilino_and_assign_departamento.py
@@ -2,7 +2,7 @@ from src.application.commands.create_inquilino_command import CreateInquilinoCom
 from src.domain.entities.inquilino import Inquilino
 from src.infrastructure.repository.inquilino_repository import InquilinoRepository
 from src.infrastructure.repository.departamento_repository import DepartamentoRepository
-
+from src.infrastructure.config.logger import logger
 
 class CreateInquilinoAndAssignDepartamento:
     def __init__(
@@ -19,9 +19,11 @@ class CreateInquilinoAndAssignDepartamento:
         departamento = self._departamento_repo.get_by_numero_departamento(command.numero_departamento)
 
         if not departamento:
+            logger.warning("El departamento no existe")
             raise ValueError("El departamento no existe")
 
         if not departamento.is_available():
+            logger.warning("El departamento ya esta ocupado")
             raise ValueError("El departamento ya esta ocupado")
 
         inquilino = Inquilino(
@@ -33,12 +35,12 @@ class CreateInquilinoAndAssignDepartamento:
 
         nuevo_id = self._inquilino_repo.create(inquilino)
 
-        print(f"Inquilino {command.nombre_completo} alojado en el departamento numero {departamento.num_departamento}")
+        logger.info(f"Inquilino {command.nombre_completo} alojado en el departamento numero {departamento.num_departamento}")
 
         # marcar departamento como ocupado
         departamento.mark_as_occupied()
 
-        print(f"Departamento numero {departamento.num_departamento} cambio a {departamento.status.value}")
+        logger.info(f"Departamento numero {departamento.num_departamento} cambio a {departamento.status.value}")
 
         self._departamento_repo.update_status(
             id_=departamento.id, status=departamento.status

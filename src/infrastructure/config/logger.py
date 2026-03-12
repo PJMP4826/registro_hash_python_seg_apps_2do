0af3cb7c88c54276b4ac0f759f0e52b2396d6927
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-03-12 11:15:44 -0600

    feat(logger): implementa configuración de logging con manejadores de archivo y consola
    - se añade un nuevo archivo logger.py para configurar el logging de la aplicación
    - se implementan manejadores para registrar mensajes en un archivo y en la consola con formato colorido
    - se establece el nivel de logging en DEBUG y se añaden mensajes de prueba para verificar la configuración

---
+++ src/infrastructure/config/logger.py
@@ -0,0 +1,51 @@
+import logging
+import colorlog
+from pathlib import Path
+
+logger = logging.getLogger("app")
+logger.setLevel(logging.DEBUG)
+
+# ruta relativa a la raiz del proyecto
+BASE_DIR = Path(__file__).resolve().parents[3]
+LOG_DIR = BASE_DIR / "logs"
+LOG_DIR.mkdir(exist_ok=True)
+
+# FILE HANDLER
+file_handler = logging.FileHandler(
+    filename=LOG_DIR / "app.log",
+    mode="w",
+    encoding="utf-8"
+)
+
+file_formatter = logging.Formatter(
+    fmt="%(asctime)s | %(levelname)s | %(filename)s:%(lineno)d | %(funcName)s | %(message)s",
+    datefmt='%m/%d/%Y %I:%M:%S %p'
+)
+
+file_handler.setFormatter(file_formatter)
+
+# COLORED CONSOLE HANDLER
+console_handler = colorlog.StreamHandler()
+
+console_formatter = colorlog.ColoredFormatter(
+    "%(log_color)s%(levelname)-8s%(reset)s | %(filename)s:%(lineno)d | %(message)s",
+    log_colors={
+        "DEBUG": "cyan",
+        "INFO": "green",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "bold_red",
+    },
+)
+
+console_handler.setFormatter(console_formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+# logger.debug("Debug message")
+# logger.info("Info message")
+# logger.warning("Warning message")
+# logger.error("Error message")
+# logger.critical("Fatal message")
+# logger.info(f"LOG_DIR: {LOG_DIR}")
